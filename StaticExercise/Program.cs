6ad726bebfb0d8d27fd366db6fe8ec33be2a5087
Keyan117 <kdaneshvari98@gmail.com>
2023-04-11 12:45:33 -0700

    update

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var celcius = TempConverter.FahrenheitToCelsius(68); // 20
+            var fahrenheit = TempConverter.CelciusToFahrenheit(20); //68
+
+            Console.WriteLine("After conversion:");
+            Console.WriteLine($"celcius {celcius}");
+            Console.WriteLine($"fahrenheit {fahrenheit}");
         }
     }
 }
