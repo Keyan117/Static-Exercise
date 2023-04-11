926a3fc9e4497694e18173d4bfcf029a2ebd0947
Keyan117 <kdaneshvari98@gmail.com>
2023-04-11 12:48:31 -0700

    update2

--- StaticExercise/TempConverter.cs
+++ StaticExercise/TempConverter.cs
@@ -4,11 +4,13 @@ namespace StaticExercise
     public static class TempConverter
     {
         public static double FahrenheitToCelsius(double fahrenheit)
-        (
+        {
             var result = (fahrenheit - 32) / 1.8;
 
             return result;
-        )
+        
+        }
+            
 
         public static double CelciusToFahrenheit(double celcius)
         {
